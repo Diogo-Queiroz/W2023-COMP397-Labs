18f7a0232701ef17b642e68cc21362061d84c6e6
Diogo Queiroz <diogoqueirooz1212@gmail.com>
2023-02-11 01:56:16 -0500

    Added AudioController, AudioSettings ScriptableObject, Singleton class, ambience and SFX added.  Fix 2 Audio Listeners from MainScene  (MiniMapCamera)

---
+++ Lab5/Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.AI.Navigation;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Serialization;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private List<NavMeshSurface> _maze;
+    [SerializeField] private List<GameObject> _robots;
+
+    private void Start()
+    {
+        foreach (var tiles in _maze)
+        {
+            tiles.BuildNavMesh();
+        }
+        foreach (var robot in _robots)
+        {
+            robot.SetActive(true);
+        }
+    }
+}
